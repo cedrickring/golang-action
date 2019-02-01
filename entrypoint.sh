4dcf676165b422452346e148ea878a212d580b4e
Patrick Reynolds <patrick.reynolds@github.com>
2019-02-01 11:14:20 -0500

    Use a symlink instead of a copy
    Symlinking files into `$GOPATH` works fine, and for non-trivial repos,
    it's a lot faster than `cp -R`.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,12 @@ if [ -z "${IMPORT}" ]; then
   IMPORT="${GITHUB_REPOSITORY}"
 fi
 WORKDIR="${GOPATH}/src/github.com/${IMPORT}"
-# create go work dir
-mkdir -p ${WORKDIR}
-# copy all files from workspace to work dir
-cp -R /github/workspace/* ${WORKDIR}
-# cd into the work dir and run all commands from there
+
+# Go can only find dependencies if they're under $GOPATH/src.
+# GitHub Actions mounts your repository outside of $GOPATH.
+# So symlink the repository into $GOPATH, and then cd to it.
+mkdir -p `dirname ${WORKDIR}`
+ln -s ${PWD} ${WORKDIR}
 cd ${WORKDIR}
 
 sh -c "$*"
