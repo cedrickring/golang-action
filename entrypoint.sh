5cd1251d9c146ec4765698a9e1286e2e75cc197a
Patrick Reynolds <patrick.reynolds@github.com>
2019-02-01 11:15:38 -0500

    Quote everything

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,8 @@ WORKDIR="${GOPATH}/src/github.com/${IMPORT}"
 # Go can only find dependencies if they're under $GOPATH/src.
 # GitHub Actions mounts your repository outside of $GOPATH.
 # So symlink the repository into $GOPATH, and then cd to it.
-mkdir -p `dirname ${WORKDIR}`
-ln -s ${PWD} ${WORKDIR}
-cd ${WORKDIR}
+mkdir -p "`dirname "${WORKDIR}"`"
+ln -s "${PWD}" "${WORKDIR}"
+cd "${WORKDIR}"
 
 sh -c "$*"
