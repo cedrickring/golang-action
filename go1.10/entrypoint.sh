4490998ee40b0a5a6fee2c7790738d0c8fc7fd20
Douglas Daniels <daniels.douglas@gmail.com>
2019-03-07 00:24:55 -0600

    Closes #2 adds support for go modules and vendoring

--- go1.10/entrypoint.sh
+++ go1.10/entrypoint.sh
@@ -7,22 +7,49 @@ if [ -z "${IMPORT}" ]; then
 fi
 WORKDIR="${GOPATH}/src/github.com/${IMPORT}"
 
+# PROJECT_PATH specifies the subdirectory in the working directory that the Go project is
+if [ -z "${PROJECT_PATH}" ]; then
+  PROJECT_PATH="."
+fi
+
 # Go can only find dependencies if they're under $GOPATH/src.
 # GitHub Actions mounts your repository outside of $GOPATH.
 # So symlink the repository into $GOPATH, and then cd to it.
 mkdir -p "$(dirname "${WORKDIR}")"
 ln -s "${PWD}" "${WORKDIR}"
-cd "${WORKDIR}"
+cd "${WORKDIR}/${PROJECT_PATH}"
 
 # If a command was specified with `args="..."`, then run it.  Otherwise,
 # look for something useful to run.
-if [ $# -eq 0 ]; then
+if [ $# -eq 0 ] || [ "$*" = "" ]; then
   if [ -r Makefile ]; then
     make
   else
+    if [ -r go.mod ]; then
+      export GO111MODULE=on
+      # Check if using vendored dependencies
+      if [ -d "vendor" ]; then
+        export GOFLAGS="-mod=vendor"
+      else
+        # Ensure no go.mod changes are made that weren't committed
+        export GOFLAGS="-mod=readonly"
+      fi
+    else
+      if [ -r Gopkg.toml ]; then
+        # Check if using vendored dependencies
+        if [ -d "vendor" ]; then
+          # Check that dep is in sync with /vendor dependencies
+          "$GOPATH/bin/dep" check
+        else
+          # Run dep ensure to download and sync dependencies
+          "$GOPATH/bin/dep" ensure
+        fi
+      fi
+    fi
     go build ./...
     go test ./...
   fi
 else
+  echo "RUNNING STUFF: [$*]"
   sh -c "$*"
 fi
